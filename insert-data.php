5020c85ba4b12f4976f0dff9a86b90b988ab70e3
noruzzamanrubel <noruzzamanrubel@gmail.com>
2021-06-15 18:14:13 +0600

    update

---
+++ insert-data.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="Responsive, HTML5 template, restaurants">
+    <meta name="author" content="" />
+
+    <!-- SITE TITLE -->
+    <title>ajax</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/main.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="row text-center my-5">
+        <table class="table">
+                <tbody>
+                    <tr>
+                        <td><h1>Php AJAX with jquery</h1></td>
+                    </tr>
+                    <tr>
+                        <td id="table-form">
+                            First Name:<input type="text" id="f_name">
+                            Last Name:<input type="text" id="l_name">
+                            <input type="submit" id="save_button" value="Submit">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td  id="table-data">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <!--  jquery-3.5.1 -->
+    <script src="assets/js/jquery-3.5.1.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Custom JS -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
